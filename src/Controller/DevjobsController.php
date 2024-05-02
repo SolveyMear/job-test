e971cf058fd751fea211a13ee520bdd4f3333c74
Cyril Gauthier <cyril.gauthier.dev@gmail.com>
2024-05-02 16:12:15 +0200

    intégration front, suite

--- src/Controller/DevjobsController.php
+++ src/Controller/DevjobsController.php
@@ -6,14 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/')]
 class DevjobsController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
+
     public function index(): Response
     {
-        // Redirige vers le fichier HTML dans le dossier public
+        // Redirige vers le dossier devjobs dans le dossier public
         return $this->redirect('devjobs/');
     }
 }
